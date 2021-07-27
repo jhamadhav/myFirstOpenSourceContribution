8ebc7db8e47439ce9687ffcd7df4d75a6f97e0ea
Jwala <rohitchaurasiya153@gmail.com>
2021-07-27 20:23:42 +0530

    Added Jwala

--- scripts/contributorList.js
+++ scripts/contributorList.js
@@ -3,4 +3,8 @@ let contributors = [
         "github": "https://github.com/jhamadhav",
         "name": "Madhav Jha"
     },
+     {
+        "github": "https://github.com/jwalapc",
+        "name": "Jwala prasad Chorasiya"
+    },
 ]
