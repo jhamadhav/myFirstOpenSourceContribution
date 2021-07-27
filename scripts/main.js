43fa4aa3ff7b96e506ddbb0950ae1ce3ce514061
jhamadhav <jhamadhav28@gmail.com>
2021-07-27 20:47:43 +0530

    logo color changed and anchor target set blank

--- scripts/main.js
+++ scripts/main.js
@@ -8,7 +8,7 @@ window.onload = () => {
                 <img src="./images/prLogo.svg" alt="PR logo">
             </td>
             <td>
-                <a href=${contributors[i].github} class="name"> ${contributors[i].name} </a>
+                <a href=${contributors[i].github} class="name" target="_blank"> ${contributors[i].name} </a>
             </td>
             <td class="rank">${i + 1}</td>
         </tr>`;
