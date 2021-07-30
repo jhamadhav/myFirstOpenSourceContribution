053e8c46570d7733637a426d3e86d219f770f784
jhamadhav <jhamadhav28@gmail.com>
2021-07-30 22:02:27 +0530

    removing demo data

--- scripts/contributorList.js
+++ scripts/contributorList.js
@@ -2,17 +2,5 @@ let contributors = [
     {
         "github": "https://github.com/jhamadhav",
         "name": "Madhav Jha"
-    },
-     {
-        "github": "https://github.com/jwalapc",
-        "name": "Jwalapc",
-    },
-    {
-        "github": "https://github.com/jwalapc",
-        "name": "Rohit",
-    },
-    {
-        "github": "https://github.com/jwalapc",
-        "name": "Suraj",
-    },
+    }
 ]
