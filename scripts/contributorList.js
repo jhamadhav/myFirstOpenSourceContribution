8dd4eb1570d4046df175eedb02929fadade77bd5
Shantanu Mane <78042851+RndmCodeGuy20@users.noreply.github.com>
2021-07-31 18:49:39 +0530

    name added : master

--- scripts/contributorList.js
+++ scripts/contributorList.js
@@ -2,5 +2,9 @@ let contributors = [
     {
         "github": "https://github.com/jhamadhav",
         "name": "Madhav Jha"
+    },
+    {
+        "github": "https://github.com/RndmCodeGuy20",
+        "name": "Shantanu Mane"
     }
 ]
