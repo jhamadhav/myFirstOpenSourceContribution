d1cdf5e6facb2ac9d198259558cc2dc99390d982
Pratik Agrawal <pratik104agrawal@gmail.com>
2021-07-31 19:29:13 +0530

    AddedMyName

--- scripts/contributorList.js
+++ scripts/contributorList.js
@@ -6,5 +6,9 @@ let contributors = [
     {
         "github": "https://github.com/jwalapc",
         "name": "Jwala "
+    },
+    {
+        "github": "https://github.com/RudraPratik30",
+        "name": "Pratik Agrawal"
     }
 ]
