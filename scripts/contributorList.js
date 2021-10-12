d4861adce678fcc0e97c8a592b1a47da30319196
Aakash Maurya <mauryaaakash916@gmail.com>
2021-10-12 19:20:23 +0530

    firstpull

--- scripts/contributorList.js
+++ scripts/contributorList.js
@@ -48,4 +48,8 @@ let contributors = [
         "github": "https://https://github.com/mk-rana",
          "name": "Mukul Rana"
     },
+    {
+        "github": "https://https://github.com/Aakashmau",
+         "name": "Aakash Maurya"
+    },
 ]
