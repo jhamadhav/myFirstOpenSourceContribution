2a1ebdcbfe6635c411a82499cb39c748766eb81c
Manoj-Murali-s <s.manojmurali@gmail.com>
2024-03-11 18:21:42 +0530

    contribution 31

--- scripts/contributorList.js
+++ scripts/contributorList.js
@@ -1,129 +1,126 @@
 let contributors = [
-    {
-        "github": "https://github.com/jhamadhav",
-        "name": "Madhav Jha"
-    },
-    {
-        "github": "https://github.com/jwalapc",
-        "name": "Jwala "
-    },
-    {
-        "github": "https://github.com/RndmCodeGuy20",
-        "name": "Shantanu Mane"
-    }
-    ,
-    {
-        "github": "https://github.com/krishna050702",
-        "name": "Krishna Mundada "
-    },
-    {
-        "github": "https://github.com/RudraPratik30",
-        "name": "Pratik Agrawal"
-    },
-    {
-        "github": "https://github.com/anushkakhandelwal18",
-        "name": "Anushka Khandelwal "
-    },
-    {
-        "github": "https://github.com/SolankiDhruvraj",
-        "name": "Dhruvraj Solanki "
-    },
-    {
-        "github": "https://github.com/khushiatcode",
-        "name": "Khushi Savaliya"
-    },
-    {
-        "github": "https://https://github.com/Dev-W32",
-        "name": "Devesh Wadhwani"
-    },
-    {
-        "github": "https://https://github.com/RamGhumaliya",
-        "name": "RamGhumaliya"
-    },
-    {
-        "github": "https://https://github.com/jwalapc",
-        "name": "JWALA CHORASIYA"
-    },
-    {
-        "github": "https://https://github.com/mk-rana",
-        "name": "Mukul Rana"
-    },
-    {
-        "github": "https://https://github.com/Aakashmau",
-        "name": "Aakash Maurya"
-    },
-    {
-        "github": "https://github.com/CT0410",
-        "name": "chetana Thorat"
-    },
-    {
-        "github": "https://https://github.com/SamPanpaliya",
-        "name": "Samiksha Panpaliya"
-
-    },
-    {
-        "github": "https://https://github.com/KIRTISINHA11",
-        "name": "Kirti Sinha"
-
-    },
-    {
-        "github": "https://github.com/JLambertazzo",
-        "name": "Julien Lambert"
-    },
-    {
-        "github": "https://github.com/Priyanshu174",
-        "name": "Priyanshu Sharma"
-    },
-    {
-        "github": "https://github.com/adi7yashah",
-        "name": "Aditya shah"
-    },
-    {
-        "github": "https://github.com/Priyanshu174",
-        "name": "Somya Gupta"
-    },
-    {
-        "github": "https://github.com/kbala97",
-        "name": "Krishna Bala"
-    },
-    {
-        "github": "https://github.com/Geetisha17",
-        "name": "Geetisha"
-    },
-    {
-        "github": "https://github.com/kbala97",
-        "name": "Gunjan Rathore"
-    },
-    {
-        "github": "https://github.com/techfreakSahil",
-        "name": "MD Sahil Khan"
-    },
-    {
-        "github": "https://github.com/Prabhat-022",
-        "name": "Prabhat Kumar"
-    }
-    ,
-    {
-        "github": "https://github.com/harshitha1201",
-        "name": "Harshitha"
-    }
-    ,{
-        "github": "https://github.com/akash7233",
-        "name": "Akash Yadav"
-    }
-    ,{
-        "github": "https://github.com/doghog125",
-        "name": "Cam B"
-    }
-    ,
-    {
-        "github": "https://github.com/Imtiaz42",
-        "name": "Imtiaz"
-    }
-    ,
-    {
-        "github": "https://github.com/UjjawalkTjNw",
-        "name": "Ujjawal"
-    }
-    ,
-]
+  {
+    github: "https://github.com/jhamadhav",
+    name: "Madhav Jha",
+  },
+  {
+    github: "https://github.com/jwalapc",
+    name: "Jwala ",
+  },
+  {
+    github: "https://github.com/RndmCodeGuy20",
+    name: "Shantanu Mane",
+  },
+  {
+    github: "https://github.com/krishna050702",
+    name: "Krishna Mundada ",
+  },
+  {
+    github: "https://github.com/RudraPratik30",
+    name: "Pratik Agrawal",
+  },
+  {
+    github: "https://github.com/anushkakhandelwal18",
+    name: "Anushka Khandelwal ",
+  },
+  {
+    github: "https://github.com/SolankiDhruvraj",
+    name: "Dhruvraj Solanki ",
+  },
+  {
+    github: "https://github.com/khushiatcode",
+    name: "Khushi Savaliya",
+  },
+  {
+    github: "https://https://github.com/Dev-W32",
+    name: "Devesh Wadhwani",
+  },
+  {
+    github: "https://https://github.com/RamGhumaliya",
+    name: "RamGhumaliya",
+  },
+  {
+    github: "https://https://github.com/jwalapc",
+    name: "JWALA CHORASIYA",
+  },
+  {
+    github: "https://https://github.com/mk-rana",
+    name: "Mukul Rana",
+  },
+  {
+    github: "https://https://github.com/Aakashmau",
+    name: "Aakash Maurya",
+  },
+  {
+    github: "https://github.com/CT0410",
+    name: "chetana Thorat",
+  },
+  {
+    github: "https://https://github.com/SamPanpaliya",
+    name: "Samiksha Panpaliya",
+  },
+  {
+    github: "https://https://github.com/KIRTISINHA11",
+    name: "Kirti Sinha",
+  },
+  {
+    github: "https://github.com/JLambertazzo",
+    name: "Julien Lambert",
+  },
+  {
+    github: "https://github.com/Priyanshu174",
+    name: "Priyanshu Sharma",
+  },
+  {
+    github: "https://github.com/adi7yashah",
+    name: "Aditya shah",
+  },
+  {
+    github: "https://github.com/Priyanshu174",
+    name: "Somya Gupta",
+  },
+  {
+    github: "https://github.com/kbala97",
+    name: "Krishna Bala",
+  },
+  {
+    github: "https://github.com/Geetisha17",
+    name: "Geetisha",
+  },
+  {
+    github: "https://github.com/kbala97",
+    name: "Gunjan Rathore",
+  },
+  {
+    github: "https://github.com/techfreakSahil",
+    name: "MD Sahil Khan",
+  },
+  {
+    github: "https://github.com/Prabhat-022",
+    name: "Prabhat Kumar",
+  },
+  {
+    github: "https://github.com/harshitha1201",
+    name: "Harshitha",
+  },
+  {
+    github: "https://github.com/akash7233",
+    name: "Akash Yadav",
+  },
+  {
+    github: "https://github.com/doghog125",
+    name: "Cam B",
+  },
+  {
+    github: "https://github.com/Imtiaz42",
+    name: "Imtiaz",
+  },
+  {
+    github: "https://github.com/UjjawalkTjNw",
+    name: "Ujjawal",
+  },
+  {
+    github: "https://github.com/Manoj-Murali-s",
+    name: "Manoj Murali S",
+  },
+];
